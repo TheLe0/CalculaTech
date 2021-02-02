dd7e6a1ef5670d78f4bc1043698d1a8859a57ae5
TheLe0 <leob.tosin@hotmail.com>
2021-02-02 20:53:53 -0300

    Implement the main events

--- app/src/main/java/com/leotosin/calculatech/MainActivity.kt
+++ app/src/main/java/com/leotosin/calculatech/MainActivity.kt
@@ -2,6 +2,7 @@ package com.leotosin.calculatech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private var userTypingNumber = false
     private var decimalDelimiterTyped = false
 
+    private var delimiterChar :Char = ','
     private var delimiter :String = ","
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +36,8 @@ class MainActivity : AppCompatActivity() {
         if (format is DecimalFormat)
         {
             val symbol :DecimalFormatSymbols = format.decimalFormatSymbols
-            delimiter = symbol.decimalSeparator.toString()
+            delimiterChar = symbol.decimalSeparator
+            delimiter = delimiterChar.toString()
         }
 
         var btnDelimiter :Button = findViewById(R.id.btnComma);
@@ -45,5 +48,77 @@ class MainActivity : AppCompatActivity() {
             getString(R.string.welcomeMessage),
             Toast.LENGTH_LONG
         ).show()
+    }
+
+    fun clickNumber(view :View)
+    {
+        val button :Button = view as Button
+        val digit :String = button.text.toString()
+        var txtVisor :TextView =  findViewById(R.id.txtNumber);
+        var currentNumber :String = txtVisor.text.toString()
+
+        if (!userTypingNumber || currentNumber == "0")
+        {
+            txtVisor.text = digit
+            if (digit != "0")
+            {
+                userTypingNumber = true
+            }
+        }
+        else
+        {
+            txtVisor.text = currentNumber.plus(digit)
+        }
+    }
+
+    fun clickOperation(view :View)
+    {
+        val button :Button = view as Button
+        val operation :String = button.text.toString()
+        var txtVisor :TextView =  findViewById(R.id.txtNumber);
+
+        if (operation == delimiter && !decimalDelimiterTyped)
+        {
+            decimalDelimiterTyped = true
+            if (!userTypingNumber)
+            {
+                txtVisor.text = "0".plus(delimiter)
+            }
+            else
+            {
+                txtVisor.text = txtVisor.text.toString().plus(delimiter)
+            }
+            userTypingNumber = true
+        }
+        else if (operation != delimiter)
+        {
+            var valueWithoutDelimiter :String = txtVisor.text.toString().replace(delimiterChar, '.')
+            calculator.operating = valueWithoutDelimiter.toDouble()
+            calculator.performOperation(operation)
+
+            var result :String = calculator.operating.toString()
+
+            if (result.endsWith(".0"))
+            {
+                result = result.substring(0, result.length - 2)
+            }
+
+            txtVisor.text = result.replace('.', delimiterChar)
+
+            userTypingNumber = false
+            decimalDelimiterTyped = false
+        }
+    }
+
+    fun clickMemory(view :View)
+    {
+        val button :Button = view as Button
+        val operation :String = button.text.toString()
+        val txtVisor :TextView =  findViewById(R.id.txtNumber);
+        val valueWithoutDelimiter :String = txtVisor.text.toString().replace(delimiterChar, '.')
+
+        calculator.operating = valueWithoutDelimiter.toDouble()
+        calculator.performMemoryOperation(operation)
+        userTypingNumber = false
     }
 }
